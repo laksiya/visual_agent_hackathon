e74a8c61b1e151c03f49cdf11f15631354ab9aa7
Håkon Skogland Halvorsen <hakon@alv.no>
2025-01-18 15:54:13 +0100

    Added llm service

--- backend/src/presentation/routers/agent_router.py
+++ backend/src/presentation/routers/agent_router.py
@@ -6,14 +6,9 @@ from ...domain.services.agent_service import ask_question_get_answer
 
 agent_router = APIRouter(prefix="/agent", tags=["Agent"])
 
-
 @agent_router.get("/ask_question", response_model=AnswerResponse)
 async def ask_question(
     question: Annotated[str, Query()]
 ) -> AnswerResponse:
     answer = await ask_question_get_answer(question=question)
     return AnswerResponse(answer=answer)
-    
-    
-    
-    
